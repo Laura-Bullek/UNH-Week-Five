f83cf4d94bf43da25da672331952dd59593140a9
LB Vik <laura.bullek@gmail.com>
2021-03-30 20:52:29 -0400

    Completed 08-Stu_Dom-Traversal project

--- 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,10 +2,16 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, select all the boxes and turn them white in a single statement.
+$(rootEl).find("li").css("background-color", "white");
+// rootEL.find("li").css("background-color", "white");
+
 // HINT: Use the `.css` function to change element's background color
+$(rootEl).find("li").css("background-color", "blue");
 
 // TODO: Starting from `rootEl`, what is the statement that will use "O" to block the "X" from winning
 // HINT: Use the `.closest()`, or `.parent()` methods to go up the DOM tree
+$(rootEl).find(".item-a3").text("O");
+
 
 // TODO: Starting from the bottom row, middle square or `.item-b3`, what is the statement that will win the game using "O"
 // HINT: Use the ".closest()", or ".parent()" methods to go up the DOM tree
